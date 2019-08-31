0f9482c3503227a6fbe84ad987f99cae1c9d278a
Alexander Staeding <alexanderstaeding@gmail.com>
2019-09-01 00:19:42 +0200

    Allow custom connection data for mongodb

--- msr-api/api-repository/src/main/java/rocks/milspecsg/msrepository/db/mongodb/MongoContext.java
+++ msr-api/api-repository/src/main/java/rocks/milspecsg/msrepository/db/mongodb/MongoContext.java
@@ -1,33 +1,36 @@
 package rocks.milspecsg.msrepository.db.mongodb;
 
-import com.google.inject.Inject;
-import com.google.inject.Singleton;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
 
-@Singleton
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 public abstract class MongoContext {
 
-    public final Datastore datastore;
+    public Datastore datastore;
 
-    @Inject
-    public MongoContext() {
-//        MongoClientURI uri = new MongoClientURI("mongodb://localhost:27017/",
-//                MongoClientOptions.builder().sslEnabled(true));
-        MongoClientURI uri = new MongoClientURI("mongodb://localhost:27017/");
+    public void init(String hostname, int port, String dbName, String user, String password) {
+        String encodedPassword = password;
+        try {
+            encodedPassword = URLEncoder.encode(password, "UTF-8");
+        } catch (UnsupportedEncodingException ignored) {
+        }
+
+        String client_url = "mongodb://" + user + ":" + encodedPassword + "@" + hostname + ":" + port + "/";
+        MongoClientURI uri = new MongoClientURI(client_url);
+
         MongoClient mongoClient = new MongoClient(uri);
 
         Morphia morphia = new Morphia();
 
         initMorphiaMaps(morphia);
 
-        datastore = morphia.createDatastore(mongoClient, getDbName());
+        datastore = morphia.createDatastore(mongoClient, dbName);
         datastore.ensureIndexes();
     }
-
-    protected abstract String getDbName();
 
     protected abstract void initMorphiaMaps(Morphia morphia);
 }
