5a55fba9b784b6df0061ea80427d9a1bfb7c9975
allenclan23 <allenclan23@gmail.com>
2020-02-08 16:00:12 -0500

    Finished ApiSpigotModule

--- anvil-spigot/src/main/java/org/anvilpowered/anvil/spigot/module/ApiSpigotModule.java
+++ anvil-spigot/src/main/java/org/anvilpowered/anvil/spigot/module/ApiSpigotModule.java
@@ -1,11 +1,29 @@
 package org.anvilpowered.anvil.spigot.module;
 
+import com.google.inject.TypeLiteral;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.anvilpowered.anvil.api.util.KickService;
+import org.anvilpowered.anvil.api.util.StringResult;
+import org.anvilpowered.anvil.api.util.TeleportationService;
+import org.anvilpowered.anvil.api.util.UserService;
 import org.anvilpowered.anvil.common.module.ApiCommonModule;
+import org.anvilpowered.anvil.spigot.util.SpigotKickService;
+import org.anvilpowered.anvil.spigot.util.SpigotStringResult;
+import org.anvilpowered.anvil.spigot.util.SpigotTeleportationService;
+import org.anvilpowered.anvil.spigot.util.SpigotUserService;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class ApiSpigotModule extends ApiCommonModule {
 
     @Override
     protected void configure() {
         super.configure();
+        bind(KickService.class).to(SpigotKickService.class);
+        bind(new TypeLiteral<StringResult<TextComponent, CommandSender>>() {
+        }).to(SpigotStringResult.class);
+        bind(TeleportationService.class).to(SpigotTeleportationService.class);
+        bind(new TypeLiteral<UserService<Player, Player>>() {
+        }).to(SpigotUserService.class);
     }
 }
