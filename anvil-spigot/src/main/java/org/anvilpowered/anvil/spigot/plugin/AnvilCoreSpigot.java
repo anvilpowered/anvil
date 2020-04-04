33df04dc05caa66fcf6e5d763440324b3c37eb6e
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2020-04-04 16:43:40 +0300

    Detect and log about Anvil/server proxy mode configuration mismatch

--- anvil-spigot/src/main/java/org/anvilpowered/anvil/spigot/plugin/AnvilCoreSpigot.java
+++ anvil-spigot/src/main/java/org/anvilpowered/anvil/spigot/plugin/AnvilCoreSpigot.java
@@ -24,6 +24,8 @@ import com.google.inject.Inject;
 import com.google.inject.Injector;
 import org.anvilpowered.anvil.api.AnvilImpl;
 import org.anvilpowered.anvil.api.Environment;
+import org.anvilpowered.anvil.api.data.config.ConfigurationService;
+import org.anvilpowered.anvil.api.data.key.Keys;
 import org.anvilpowered.anvil.api.plugin.Plugin;
 import org.anvilpowered.anvil.common.plugin.AnvilCore;
 import org.anvilpowered.anvil.common.plugin.AnvilCorePluginInfo;
@@ -31,9 +33,14 @@ import org.anvilpowered.anvil.spigot.listener.SpigotPlayerListener;
 import org.anvilpowered.anvil.spigot.module.ApiSpigotModule;
 import org.anvilpowered.anvil.spigot.module.SpigotModule;
 import org.bukkit.Bukkit;
+import org.bukkit.Server;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Set;
+import java.util.logging.Level;
 
 public class AnvilCoreSpigot extends JavaPlugin implements Plugin<JavaPlugin> {
 
@@ -52,8 +59,54 @@ public class AnvilCoreSpigot extends JavaPlugin implements Plugin<JavaPlugin> {
 
     private static final class Inner extends AnvilCore<JavaPlugin> {
         @Inject
+        private JavaPlugin plugin;
+
+        @Inject
         public Inner(Injector injector) {
             super(injector, new SpigotModule());
+        }
+
+        @Override
+        protected void whenLoaded(Environment environment) {
+            super.whenLoaded(environment);
+
+            // Attempt to autodetect proxy mode
+            boolean serverProxyMode = false;
+
+            // Check for Paper-specific Velocity mode first
+            try {
+                Method getPaperConfigMethod = Server.Spigot.class.getMethod("getPaperConfig");
+                YamlConfiguration paperConfig = (YamlConfiguration) getPaperConfigMethod
+                    .invoke(Bukkit.spigot());
+                serverProxyMode = paperConfig.getBoolean("settings.velocity-support.enabled", false);
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                throw new RuntimeException("Unable to get Paper configuration", e);
+            } catch (NoSuchMethodException ignored) {
+            }
+
+            // Check BungeeCord mode if Velocity setting was not enabled or server implementation is not Paper
+            if (!serverProxyMode) {
+                serverProxyMode = Bukkit.spigot().getConfig()
+                    .getBoolean("settings.bungeecord", false);
+            }
+
+            ConfigurationService configurationService = environment.getInjector()
+                .getInstance(ConfigurationService.class);
+            boolean anvilProxyMode = configurationService.getOrDefault(Keys.PROXY_MODE);
+
+            if (serverProxyMode && !anvilProxyMode) {
+                plugin.getLogger().log(
+                    Level.SEVERE,
+                    "It looks like you are running this server behind a proxy. " +
+                        "If this is the case, set server.proxyMode=true in the anvil config"
+                );
+            } else if (anvilProxyMode && !serverProxyMode) {
+                plugin.getLogger().log(
+                    Level.SEVERE,
+                    "It looks like you are not running this server behind a proxy. " +
+                        "If this is the case, set server.proxyMode=false in the anvil config"
+                );
+            }
         }
     }
 
