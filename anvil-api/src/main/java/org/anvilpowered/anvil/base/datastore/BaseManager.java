93172c5d7018715fdd2d9735147b7bce8eac6713
Alexander Staeding <alexanderstaeding@gmail.com>
2020-04-26 11:24:12 +0200

    Fix issue with generic types not being resolved in BaseManager

--- anvil-api/src/main/java/org/anvilpowered/anvil/base/datastore/BaseManager.java
+++ anvil-api/src/main/java/org/anvilpowered/anvil/base/datastore/BaseManager.java
@@ -19,28 +19,34 @@
 package org.anvilpowered.anvil.base.datastore;
 
 import com.google.common.reflect.TypeToken;
-import com.google.inject.ConfigurationException;
+import com.google.inject.Binding;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
 import com.google.inject.Key;
+import com.google.inject.name.Named;
 import com.google.inject.name.Names;
 import org.anvilpowered.anvil.api.Anvil;
 import org.anvilpowered.anvil.api.data.key.Keys;
 import org.anvilpowered.anvil.api.data.registry.Registry;
 import org.anvilpowered.anvil.api.datastore.Component;
 import org.anvilpowered.anvil.api.datastore.Manager;
-import org.anvilpowered.anvil.api.misc.BindingExtensions;
 
 import java.util.Locale;
+import java.util.Map;
 import java.util.Objects;
 
+@SuppressWarnings({"UnstableApiUsage", "unchecked"})
 public abstract class BaseManager<C extends Component<?, ?>> implements Manager<C> {
 
     protected Registry registry;
 
+    private final TypeToken<C> componentType;
+
     protected BaseManager(Registry registry) {
         this.registry = registry;
         registry.whenLoaded(this::registryLoaded);
+        componentType = new TypeToken<C>(getClass()) {
+        };
     }
 
     @Inject
@@ -55,14 +61,20 @@ public abstract class BaseManager<C extends Component<?, ?>> implements Manager<
     private void loadComponent() {
         String dataStoreName = Anvil.resolveForSharedEnvironment(Keys.DATA_STORE_NAME, registry)
             .toLowerCase(Locale.ENGLISH);
-        try {
-            currentComponent = injector.getInstance(Key.get(BindingExtensions.getTypeLiteral(new TypeToken<C>(getClass()) {
-            }), Names.named(dataStoreName)));
-        } catch (IllegalStateException | ConfigurationException e) {
-            String message = "Anvil: Could not find requested data store: \"" + dataStoreName + "\". Did you bind it correctly?";
-            System.err.println(message);
-            throw new IllegalStateException(message, e);
+        String type = componentType.getRawType().getCanonicalName();
+        Named named = Names.named(dataStoreName);
+        for (Map.Entry<Key<?>, Binding<?>> entry : injector.getBindings().entrySet()) {
+            Key<?> k = entry.getKey();
+            if (k.getTypeLiteral().getType().getTypeName().contains(type)
+                && named.equals(k.getAnnotation())) {
+                currentComponent = (C) entry.getValue().getProvider().get();
+                return;
+            }
         }
+        String message = "Anvil: Could not find requested data store: \"" + dataStoreName
+            + "\". Did you bind it correctly?";
+        System.err.println(message);
+        throw new IllegalStateException(message);
     }
 
     @Override
@@ -71,9 +83,12 @@ public abstract class BaseManager<C extends Component<?, ?>> implements Manager<
             if (currentComponent == null) {
                 loadComponent();
             }
-            return Objects.requireNonNull(currentComponent, "An error occurred while loading current component");
+            return Objects.requireNonNull(currentComponent,
+                "An error occurred while loading current component");
         } catch (RuntimeException e) {
-            String message = "Anvil: DataStoreName has not been loaded yet! Make sure your Registry and ConfigurationService implementations are annotated with @Singleton!";
+            String message = "Anvil: DataStoreName has not been loaded yet!" +
+                "Make sure your Registry and ConfigurationService implementations" +
+                "are annotated with @Singleton!";
             System.err.println(message);
             throw new IllegalStateException(message, e);
         }
