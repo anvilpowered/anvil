745e982ca7d0b2529e785102d1b7df0c08b9af16
Alexander Staeding <alexanderstaeding@gmail.com>
2021-01-28 13:39:54 +0100

    Its 2021

--- anvil-nukkit/src/main/java/org/anvilpowered/anvil/nukkit/entity/NukkitEntityUtils.java
+++ anvil-nukkit/src/main/java/org/anvilpowered/anvil/nukkit/entity/NukkitEntityUtils.java
@@ -1,6 +1,6 @@
 /*
- * Anvil - AnvilPowered
- *   Copyright (C) 2020
+ *   Anvil - AnvilPowered
+ *   Copyright (C) 2020-2021
  *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU Lesser General Public License as published by
@@ -13,7 +13,7 @@
  *     GNU Lesser General Public License for more details.
  *
  *     You should have received a copy of the GNU Lesser General Public License
- *     along with this program.  If not, see https://www.gnu.org/licenses/.
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package org.anvilpowered.anvil.nukkit.entity;
