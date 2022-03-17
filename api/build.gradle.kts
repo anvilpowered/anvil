ee021796352d1d39e0f955422628678e20358b74
Alexander Staeding <alexanderstaeding@gmail.com>
2022-03-17 02:21:44 +0100

    Include reflections in artifact

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -28,7 +28,7 @@ dependencies {
     compileOnlyApi("org.spongepowered:configurate-hocon:$configurateVersion")
     compileOnlyApi("org.spongepowered:math:$spongeMathVersion")
     api("org.jetbrains.xodus:xodus-entity-store:$xodusVersion")
-    compileOnlyApi("org.reflections:reflections:$reflectionsVersion")
+    api("org.reflections:reflections:$reflectionsVersion")
     api("org.jetbrains:annotations:$jetbrainsAnnotationsVersion")
     api("org.anvilpowered:registry:0.1.0-SNAPSHOT")
 }
