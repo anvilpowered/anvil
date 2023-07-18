54fb20c644afceae77962faf7f20c9d724ab457e
Alexander Städing <alexanderstaeding@gmail.com>
2023-07-18 15:16:00 +0200

    Update for paper changes

--- app/plugin/paper/src/jvmMain/kotlin/org/anvilpowered/anvil/plugin/AnvilPaperPlugin.kt
+++ app/plugin/paper/src/jvmMain/kotlin/org/anvilpowered/anvil/plugin/AnvilPaperPlugin.kt
@@ -4,14 +4,14 @@ package org.anvilpowered.anvil.plugin
 
 import io.papermc.paper.event.server.ServerResourcesLoadEvent
 import org.anvilpowered.anvil.AnvilPaperApi
-import org.anvilpowered.anvil.command.toPaperRoot
+import org.anvilpowered.anvil.command.toPaper
 import org.bukkit.plugin.Plugin
 
 context(AnvilPaperApi)
 class AnvilPaperPlugin : AnvilPlugin() {
     fun registerCommands(bootstrap: Plugin, event: ServerResourcesLoadEvent) {
         registerCommands { command ->
-            event.commands.register(command.toPaperRoot(bootstrap))
+            event.commands.register(bootstrap, command.toPaper())
         }
     }
 }
--- app/plugin/paper/src/jvmMain/kotlin/org/anvilpowered/anvil/plugin/AnvilPaperPluginBootstrap.kt
+++ app/plugin/paper/src/jvmMain/kotlin/org/anvilpowered/anvil/plugin/AnvilPaperPluginBootstrap.kt
@@ -2,9 +2,12 @@
 
 package org.anvilpowered.anvil.plugin
 
+import com.mojang.brigadier.builder.LiteralArgumentBuilder
+import io.papermc.paper.command.brigadier.CommandSourceStack
 import io.papermc.paper.event.server.ServerResourcesLoadEvent
 import org.anvilpowered.anvil.api.AnvilApi
 import org.anvilpowered.anvil.createPaper
+import org.anvilpowered.anvil.domain.user.Component
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.plugin.java.JavaPlugin
