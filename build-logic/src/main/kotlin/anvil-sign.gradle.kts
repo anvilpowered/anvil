726e5143901e982b644d4b38c930202d00d2cddd
Alexander Städing <alexanderstaeding@gmail.com>
2023-12-10 18:02:55 +0100

    Signatures are still not being uploaded

--- build-logic/src/main/kotlin/anvil-sign.gradle.kts
+++ build-logic/src/main/kotlin/anvil-sign.gradle.kts
@@ -1,24 +1,17 @@
-import org.jetbrains.kotlin.gradle.tooling.BuildKotlinToolingMetadataTask
+import gradle.kotlin.dsl.accessors._0ac9a36cec4eeb1254edca678008b431.publishing
 
 plugins {
     signing
 }
 
-extensions.configure<PublishingExtension> {
+extensions.configure<SigningExtension> {
     if (System.getenv("AGENT_NAME")?.contains("publishing") != true) {
         return@configure
     }
-    extensions.configure<SigningExtension> {
-        useInMemoryPgpKeys(
-            /* defaultKeyId = */ System.getenv("SIGNING_KEY_ID"),
-            /* defaultSecretKey = */ System.getenv("SIGNING_KEY"),
-            /* defaultPassword = */ System.getenv("SIGNING_PASSWORD"),
-        )
-        publications.withType<MavenPublication> {
-            val signingTasks = sign(this)
-            tasks.withType<AbstractPublishToMaven> {
-                dependsOn(signingTasks)
-            }
-        }
-    }
+    useInMemoryPgpKeys(
+        /* defaultKeyId = */ System.getenv("SIGNING_KEY_ID"),
+        /* defaultSecretKey = */ System.getenv("SIGNING_KEY"),
+        /* defaultPassword = */ System.getenv("SIGNING_PASSWORD"),
+    )
+    sign(publishing.publications)
 }
