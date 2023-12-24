90229c24990bab8c1cbb62c56975e717ad21e437
Alexander Städing <alexanderstaeding@gmail.com>
2023-12-24 22:58:21 +0100

    Refactor player arguments with new CommandExecutionScope DSL

--- velocity/src/main/kotlin/org/anvilpowered/anvil/velocity/command/PlayerCommand.kt
+++ velocity/src/main/kotlin/org/anvilpowered/anvil/velocity/command/PlayerCommand.kt
@@ -26,39 +26,73 @@ import net.kyori.adventure.text.format.NamedTextColor
 import org.anvilpowered.kbrig.argument.StringArgumentType
 import org.anvilpowered.kbrig.builder.ArgumentBuilder
 import org.anvilpowered.kbrig.builder.RequiredArgumentBuilder
-import org.anvilpowered.kbrig.builder.executesSuspending
 import org.anvilpowered.kbrig.context.CommandContext
+import org.anvilpowered.kbrig.context.CommandExecutionScope
+import org.anvilpowered.kbrig.context.CommandExecutionScopeDsl
+import org.anvilpowered.kbrig.context.executesScoped
 import org.anvilpowered.kbrig.context.get
+import org.anvilpowered.kbrig.context.yieldError
 import kotlin.jvm.optionals.getOrNull
 
-fun ArgumentBuilder.Companion.requirePlayer(
+fun ArgumentBuilder.Companion.requirePlayerArgument(
     proxyServer: ProxyServer,
     argumentName: String = "player",
     command: suspend (context: CommandContext<CommandSource>, player: Player) -> Int,
 ): RequiredArgumentBuilder<CommandSource, String> =
-    required<CommandSource, String>(argumentName, StringArgumentType.SingleWord)
+    requirePlayerArgumentBase(proxyServer)
+        .executesScoped {
+            val player = extractPlayerArgument(proxyServer, argumentName)
+            yield(command(context, player))
+        }
+
+fun ArgumentBuilder.Companion.requirePlayerArgumentScoped(
+    proxyServer: ProxyServer,
+    argumentName: String = "player",
+    command: suspend CommandExecutionScope<CommandSource>.(player: Player) -> Unit,
+): RequiredArgumentBuilder<CommandSource, String> =
+    requirePlayerArgumentBase(proxyServer)
+        .executesScoped {
+            val player = extractPlayerArgument(proxyServer, argumentName)
+            command(player)
+        }
+
+private fun ArgumentBuilder.Companion.requirePlayerArgumentBase(
+    proxyServer: ProxyServer,
+): RequiredArgumentBuilder<CommandSource, String> =
+    required<CommandSource, String>("player", StringArgumentType.SingleWord)
         .suggests { _, builder ->
             proxyServer.allPlayers.forEach { player -> builder.suggest(player.username) }
             builder.build()
-        }.executesSuspending { context ->
-            context.requirePlayer(proxyServer, argumentName) { command(context, it) }
         }
 
-suspend fun CommandContext<CommandSource>.requirePlayer(
+@CommandExecutionScopeDsl
+suspend fun CommandExecutionScope<CommandSource>.extractPlayerArgument(
     proxyServer: ProxyServer,
     argumentName: String = "player",
-    command: suspend (player: Player) -> Int,
-): Int {
-    val playerName = get<String>(argumentName)
-    return proxyServer.getPlayer(playerName).getOrNull()?.let { velocityPlayer ->
-        command(velocityPlayer)
-    } ?: run {
-        source.sendMessage(
+): Player {
+    val playerName = context.get<String>(argumentName)
+    val player = proxyServer.getPlayer(playerName).getOrNull()
+    if (player == null) {
+        context.source.sendMessage(
             Component.text()
                 .append(Component.text("Player with name ", NamedTextColor.RED))
                 .append(Component.text(playerName, NamedTextColor.GOLD))
                 .append(Component.text(" not found!", NamedTextColor.RED)),
         )
-        0
+        yieldError()
     }
+    return player
+}
+
+@CommandExecutionScopeDsl
+suspend fun CommandExecutionScope<CommandSource>.extractPlayerSource(): Player {
+    val player = context.source as? Player
+    if (player == null) {
+        context.source.sendMessage(
+            Component.text()
+                .append(Component.text("You must be a player to use this command!", NamedTextColor.RED)),
+        )
+        yieldError()
+    }
+    return player
 }
